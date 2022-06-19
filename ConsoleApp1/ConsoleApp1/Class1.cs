fd3043da9cedccc94a380ed0de9633f7bbfb6298
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-19 12:42:16 +0530

    Commiting fils to UC2

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -17,6 +17,23 @@ namespace ConsoleApp1
         public long phoneNumber;
         public String email;
 
+        public AddressBook(string firstName, String lastName,String address,String city,String state,long zip,long phoneNumber,String email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public AddressBook()
+        {
+
+        }
+
         static public void SetAddress()
         {
 
